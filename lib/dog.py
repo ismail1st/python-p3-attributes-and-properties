02df84a9e66e8043fd045294f1527357ce74d546
Ismail Hassan <ismaylhassan8@gmail.com>
2025-11-25 14:30:55 +0300

    lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,36 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        if name is not None:
+            self.name = name
+        if breed is not None:
+            self.breed = breed
+
+    # --------------------
+    # NAME PROPERTY
+    # --------------------
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if type(value) is not str or len(value) < 1 or len(value) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value
+
+    # --------------------
+    # BREED PROPERTY
+    # --------------------
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = value
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,36 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name=None, job=None):
+        if name is not None:
+            self.name = name
+        if job is not None:
+            self.job = job
+
+    # --------------------
+    # NAME PROPERTY
+    # --------------------
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if type(value) is not str or len(value) < 1 or len(value) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value.title()
+
+    # --------------------
+    # JOB PROPERTY
+    # --------------------
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = value
